54bffb97c39409af9a038c4a1d4131231d1ef1c9
Mohamad Alamin <alamin.mohamad.a0@gmail.com>
2023-05-22 01:19:57 +0200

    Done with all of what the JS should have, and edited the style.css a little, still have the good to have part

--- app.js
+++ app.js
@@ -1,48 +1,212 @@
-async function fetchData(cryptoCurrency) {
+/*
+-----------------------------------------------------------------------------------------------
+-----------------------------------------------------------------------------------------------
+---------------------------------------- Coin Search ------------------------------------------
+-----------------------------------------------------------------------------------------------
+-----------------------------------------------------------------------------------------------
+*/
+
+function coinSearch() {
+  const searchInput = document.getElementById('crypto-search-input');
+  const searchTerm = searchInput.value.trim().toLowerCase();
+
+  if (searchTerm.length === 0) {
+    renderResults([]);
+    document.getElementById('errorMessage').innerHTML = '';
+    return;
+  }
+
+  const url = `https://api.coingecko.com/api/v3/coins/list`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((coinsData) => {
+      const filteredResults = coinsData.filter(
+        (coin) =>
+          coin.name.toLowerCase().includes(searchTerm) ||
+          coin.symbol.toLowerCase().includes(searchTerm)
+      );
+      const results = filteredResults.map(
+        (coin) => `${coin.name} (${coin.symbol})`
+      );
+      renderResults(results);
+      document.getElementById('errorMessage').innerHTML = '';
+    })
+    .catch((error) => {
+      document.getElementById('errorMessage').innerHTML = new Error(
+        'Failed to fetch data'
+      );
+      renderResults([]);
+    });
+}
+
+function fetchCoinDetails(coinName) {
+  const url = `https://api.coingecko.com/api/v3/coins/${coinName.toLowerCase()}`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((coinDetails) => {
+      displayCoinDetails(coinDetails);
+    })
+    .catch((error) => console.error(error));
+}
+
+function displayCoinDetails(coinData) {
+  const coinRank = coinData.market_cap_rank;
+  const coinName = coinData.name;
+  const coinSymbol = coinData.symbol
+    ? coinData.symbol.toString().toUpperCase()
+    : '';
+  const coinThumb = coinData.image?.small || '';
+  const coinCurrentPrice = coinData.market_data.current_price.usd;
+  const coinToBtc = coinData.market_data.current_price.btc;
+  const coinTotalSupply = coinData.market_data.total_supply.toLocaleString();
+  const coinLastUpdated = coinData.market_data.last_updated;
+  const coinDescription = coinData.description?.en;
+  const coinToEur = coinData.market_data.current_price.eur;
+  const coinToAud = coinData.market_data.current_price.aud;
+  const coinToInr = coinData.market_data.current_price.inr;
+
+  const detailsContainer = document.getElementById('coin-details');
+  detailsContainer.innerHTML = `
+    <h6><span style="background-color: black; color: white; border-radius: 10px; padding: 5px;"> Rank: #${coinRank} </span></h6> <br>
+    <h3><img src="${coinThumb}" alt="${coinName}" style="width: 25px"/> ${coinName} (${coinSymbol})</h3>
+    <h4>Current Price: $${coinCurrentPrice}</h4>
+    <h6>${coinToBtc} BTC</h6><br>
+    <h5>Total Supply: ${coinTotalSupply}</h5>
+    <h5>Last Updated: ${coinLastUpdated}</h5><br>
+    <p style="font-weight: bold;">Description:</p>
+    <p>${coinDescription}</p><br>
+    <h4 style="font-weight: bold;">Global Helium Prices:</h4><br>
+  <h4>HNT / EUR: €${coinToEur}</h4><br>
+  <h4>HNT / AUD: A$${coinToAud}</h4><br>
+  <h4>HNT / INR: ₹${coinToInr}</h4><br>
+    
+  `;
+}
+
+function renderResults(results) {
+  const menu = document.getElementById('search-results');
+  const searchInput = document.getElementById('crypto-search-input');
+  menu.innerHTML = '';
+
+  if (results.length > 0) {
+    results.slice(0, 10).forEach((result) => {
+      const item = document.createElement('li');
+      item.innerText = result;
+      item.addEventListener('click', () => {
+        searchInput.value = result;
+        menu.style.display = 'none';
+        const coinName = result.split(' (')[0];
+        fetchCoinDetails(coinName);
+      });
+      menu.appendChild(item);
+    });
+
+    menu.style.display = 'block';
+  } else {
+    if (searchInput.value.trim().length > 0) {
+      const noResultsItem = document.createElement('li');
+      noResultsItem.innerText = 'No results found';
+      menu.appendChild(noResultsItem);
+      menu.style.display = 'block';
+    } else {
+      menu.style.display = 'none';
+    }
+  }
+}
+
+let searchTimeoutToken = 0;
+window.onload = () => {
+  const searchInput = document.getElementById('crypto-search-input');
+  const coinDetailsContainer = document.getElementById('coin-details');
+
+  searchInput.onkeyup = (event) => {
+    clearTimeout(searchTimeoutToken);
+
+    if (searchInput.value.trim().length === 0) {
+      renderResults([]);
+      coinDetailsContainer.innerHTML = ''; // Clear the coin details container
+      return;
+    }
+
+    setTimeout(() => {
+      coinSearch();
+    }, 370);
+  };
+
+  searchInput.addEventListener('search', () => {
+    if (searchInput.value.trim().length === 0) {
+      renderResults([]);
+      coinDetailsContainer.innerHTML = ''; // Clear the coin details container
+    }
+  });
+  searchInput.addEventListener('click', () => {
+    coinSearch();
+  });
+};
+
+/*
+-----------------------------------------------------------------------------------------------
+-----------------------------------------------------------------------------------------------
+------------------------------------ Table Coin Data ------------------------------------------
+-----------------------------------------------------------------------------------------------
+-----------------------------------------------------------------------------------------------
+*/
+
+async function fetchData() {
   try {
-    const url = `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=${cryptoCurrency}`;
+    const url =
+      'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false&price_change_percentage=1h%2C24h%2C7d&locale=en';
     const response = await fetch(url);
 
     if (!response.ok) {
       throw new Error('Failed to fetch data');
     }
+
     const data = await response.json();
-    console.log(data);
     return data;
   } catch (error) {
     console.log(`Error: ${error.message}`);
     throw error;
   }
 }
+fetchData();
+async function updateTable() {
+  const tableBody = document.getElementById('table-body');
+  tableBody.innerHTML = '';
 
-fetchData('helium');
+  try {
+    const coinData = await fetchData();
+    coinData.forEach((coin, index) => {
+      const newRow = createTableRow(index + 1, coin);
+      tableBody.appendChild(newRow);
+    });
+  } catch (error) {
+    console.log(`Error: ${error.message}`);
+  }
+}
 
-//	List all coins with id, name, and symbol
-// async function listData() {
-//   try {
-//     const url = `https://api.coingecko.com/api/v3/coins/list?include_platform=true`;
-//     const response = await fetch(url);
+function createTableRow(rank, data) {
+  const row = document.createElement('tr');
+  console.log(data);
+  row.innerHTML = `
+    <td>${rank}</td>
+    <td><img src="${data.image}" alt="${
+    data.name
+  }" width="13"> <span class="coinName"><a style="color: black; text-decoration: none;" href="#" onclick="fetchCoinDetails('${
+    data.id
+  }')">${data.name}</a></span></td>
+    <td>$${data.current_price.toLocaleString()}</td>
+    <td>${data.price_change_percentage_1h_in_currency.toFixed(2)}%</td>
+    <td>${data.price_change_percentage_24h.toFixed(2)}%</td>
+    <td>${data.price_change_percentage_7d_in_currency.toFixed(2)}%</td>
+    <td>$${data.total_volume.toLocaleString()}</td>
+    <td>$${data.market_cap.toLocaleString()}</td>
+    <td>${data.last_updated}</td>
+  `;
+  return row;
+}
 
-//     if (!response.ok) {
-//       throw new Error('Failed to fetch data');
-//     }
+updateTable();
 
-//     const data = await response.json();
-//     console.log(data);
-//     return data;
-//   } catch (error) {
-//     console.log(`Error: ${error.message}`);
-//     throw error;
-//   }
-// }
-
-// listData('helium');
-
-// https://api.coingecko.com/api/v3/coins/helium //	Get current data for a coin
-
-// https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en
-// List all coins with market data
-
-// https://api.coingecko.com/api/v3/nfts/list // nft list
-
-// https://api.coingecko.com/api/v3/search?query=helium // List of coins, categories and markets matching search term ordered by market cap
+// TODO : Display the chart of the coin :( the API link for the chart :
+// TODO : https://api.coingecko.com/api/v3/coins/helium/market_chart?vs_currency=usd&days=1
